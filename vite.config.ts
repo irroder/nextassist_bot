329a0d1ca97cebec0e36e76dcf6becd4b4b45756
irroder <mik.ushakoff2017@yandex.ru>
2025-04-26 19:50:17 +0300

    refactor: simplify Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,24 +1,11 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import * as fs from "node:fs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
-	optimizeDeps: {
-		exclude: ["lucide-react"],
-	},
 	server: {
-		port: 443,
+		port: 3000,
 		host: true,
-		hmr: {
-			host: "localhost",
-			port: 443,
-		},
-		https: {
-			key: fs.readFileSync("./.cert/localhost-key.pem"),
-			cert: fs.readFileSync("./.cert/localhost.pem"),
-		},
-		cors: true,
 	},
 });
